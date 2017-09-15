1eab8d86bd5f375a88f9afdbc80b88d3968d8f7a
DaVinci42 <starrynight.ying@gmail.com>
2017-09-15 16:10:59 +0800

    optimize

--- README.md
+++ README.md
@@ -2,11 +2,14 @@
 
   ```kotlin
   val d = GradientMotionDrawable(1000)
-  d.addDrawable(GradientDrawable.Orientation.BL_TR, Color.parseColor("#f44336"), Color.parseColor("#F4511E"))
-  d.addDrawable(GradientDrawable.Orientation.BR_TL, Color.parseColor("#0288D1"), Color.parseColor("#C2185B"))
-  d.addDrawable(GradientDrawable.Orientation.BR_TL, Color.parseColor("#424242"), Color.parseColor("#673AB7"))
+          .withDrawables(arrayListOf(
+                  Triple(GradientDrawable.Orientation.BL_TR, Color.parseColor("#f44336"), Color.parseColor("#F4511E")),
+                  Triple(GradientDrawable.Orientation.BR_TL, Color.parseColor("#0288D1"), Color.parseColor("#C2185B")),
+                  Triple(GradientDrawable.Orientation.BR_TL, Color.parseColor("#424242"), Color.parseColor("#673AB7"))
+          ))
   iv.setImageDrawable(d)
   d.start()
   ```
 
   ​
+
--- androidutils/src/main/java/com/davinci42/androidutils/ui/GradientMotionDrawable.kt
+++ androidutils/src/main/java/com/davinci42/androidutils/ui/GradientMotionDrawable.kt
@@ -1,4 +1,4 @@
-package com.davinci42.androidutils.image
+package com.davinci42.androidutils.ui
 
 import android.graphics.drawable.AnimationDrawable
 import android.graphics.drawable.GradientDrawable
@@ -14,9 +14,11 @@ class GradientMotionDrawable(private val durationInMs: Int, isOneShot: Boolean =
         this.isOneShot = isOneShot
     }
 
-    fun addDrawable(orientation: GradientDrawable.Orientation, startColor: Int, endColor: Int): GradientMotionDrawable {
-        val drawable = GradientDrawable(orientation, intArrayOf(startColor, endColor))
-        this.addFrame(drawable, durationInMs)
+    fun withDrawables(drawables: List<Triple<GradientDrawable.Orientation, Int, Int>>): GradientMotionDrawable {
+        drawables.forEach {
+            val drawable = GradientDrawable(it.first, intArrayOf(it.second, it.third))
+            this.addFrame(drawable, durationInMs)
+        }
         return this
     }
 }
